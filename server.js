a093e8f011fb5e96879129201a354881e53bb9c1
Jorge Campos <jorge.camposserrano@gmail.com>
2014-11-23 18:26:42 +0100

    Add server

---
+++ server.js
@@ -0,0 +1,7 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(2013);
+console.log('Server ready in http://localhost:2013');
