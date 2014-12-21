7b52742196e0e62b20963309d9773e39bcdb6151
Jorge Campos <jorge.camposserrano@gmail.com>
2014-12-21 17:31:38 +0100

    Create WebGLObject library

--- js/objgraph.js
+++ js/objgraph.js
@@ -1,104 +1,151 @@
-function ObjGraph(width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff){
-    this.width = width;
-    this.height = height;
-    this.posX = x;
-    this.posY = y;
-    this.vbuff = vbuff;
-    this.ibuff = ibuff;
-    this.txtbuff = txtbuff;
-    this.txtid = txtid;
-    this.nbuff = nbuff;
-}
+/**
+ * WebGL Object Module
+ */
+var WebGLObject = (function () {
 
-ObjGraph.prototype.draw = function() {
-    pushMatrix();
+	var module = {};
 
-    mat4.translate(mvMatrix, [this.posX, this.posY, 0.0]);
-    mat4.toInverseMat3(mvMatrix, normalMatrix);
-    mat3.transpose(normalMatrix);
-  
-    // set the buffer to be drawn and connect up the shader parameters: 
-    // vertex position, texture coordinates and vertex normal
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
-    gl.vertexAttribPointer(glProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-  
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.txtbuff);
-    gl.vertexAttribPointer(glProgram.vertexTextureAttribute, 2, gl.FLOAT, false, 0, 0);
-  
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.nbuff);
-    gl.vertexAttribPointer(glProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+	var gl;
+	var glProgram;
+	var textures;
 
-    gl.uniformMatrix4fv(glProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(glProgram.mvMatrixUniform, false, mvMatrix);
-    gl.uniformMatrix3fv(glProgram.normalMatrixUniform, false, normalMatrix);
+	var matrix;
 
-    gl.activeTexture(gl.TEXTURE0 + this.txtid);
-    gl.bindTexture(gl.TEXTURE_2D, textures[this.txtid]);
-    gl.uniform1i(glProgram.samplerUniform, this.txtid);
-  
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibuff);
-    gl.drawElements(gl.TRIANGLES, this.ibuff.number_vertex_points, gl.UNSIGNED_SHORT, 0);
-    
-    popMatrix();
-}
+	var objects = {};
 
-function Ball(width, height, x, y, speed, vbuff, ibuff, txtbuff, txtid, nbuff) {
-    ObjGraph.call(this, width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff);
-    this.DirX = 1;
-    this.DirY = 1;
-    this.speed = speed;
-}
-Ball.prototype = new ObjGraph();
+	function ObjGraph(width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff){
+		this.width = width;
+		this.height = height;
+		this.posX = x;
+		this.posY = y;
+		this.vbuff = vbuff;
+		this.ibuff = ibuff;
+		this.txtbuff = txtbuff;
+		this.txtid = txtid;
+		this.nbuff = nbuff;
+	};
 
-Ball.prototype.move = function() {
-    var limitX = Settings.field.width / 2.0 - 0.8*this.width;
-    var limitY = Settings.field.height / 2.0 - 0.8*this.width;
+	ObjGraph.prototype.draw = function() {
+		WebGLUtils.pushMatrix();
 
-    if (this.posX >= limitX || this.posX <= -limitX) {
-        this.DirX = -this.DirX;
-    }
-    if (this.posY >= limitY || this.posY <= -limitY) {
-        this.DirY = -this.DirY;
-    }
+		mat4.translate(matrix.mv, [this.posX, this.posY, 0.0]);
+		mat4.toInverseMat3(matrix.mv, matrix.n);
+		mat3.transpose(matrix.n);
 
-    // update ball position
-    this.posX += this.DirX * this.speed;
-    this.posY += this.DirY * this.speed;
-}
+		// set the buffer to be drawn and connect up the shader parameters: 
+		// vertex position, texture coordinates and vertex normal
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
+		gl.vertexAttribPointer(glProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
 
-function Paddle(width, height, x, y, speed, vbuff, ibuff, txtbuff, txtid, nbuff) {
-    ObjGraph.call(this, width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff);
-    this.speed = speed;
-}
-Paddle.prototype = new ObjGraph();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.txtbuff);
+		gl.vertexAttribPointer(glProgram.vertexTextureAttribute, 2, gl.FLOAT, false, 0, 0);
 
-Paddle.prototype.move = function() {
-    var limitX = Settings.field.width / 2.0 - this.width;
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.nbuff);
+		gl.vertexAttribPointer(glProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
 
-    // move right
-    if (Key.isDown(Key.RIGHT) || (Key.isDown(Key.D))) {
-        if (this.posX <= limitX) {
-            // update paddle position
-            this.posX += this.speed;
-        }
+		gl.uniformMatrix4fv(glProgram.pMatrixUniform, false, matrix.p);
+		gl.uniformMatrix4fv(glProgram.mvMatrixUniform, false, matrix.mv);
+		gl.uniformMatrix3fv(glProgram.normalMatrixUniform, false, matrix.n);
 
-    }
-    // move left
-    else if (Key.isDown(Key.LEFT) || (Key.isDown(Key.A))) {
-        if (this.posX >= -limitX) {
-            // update paddle position
-            this.posX -= this.speed;
-        }
-    }
-}
+		gl.activeTexture(gl.TEXTURE0 + this.txtid);
+		gl.bindTexture(gl.TEXTURE_2D, textures[this.txtid]);
+		gl.uniform1i(glProgram.samplerUniform, this.txtid);
 
-Paddle.prototype.logic = function() {
-    if (ball.posX <= this.posX + Settings.paddle.width / 2.0  &&
-            ball.posX >= this.posX - Settings.paddle.width / 2.0) {
-        if (ball.posY - Settings.ball.width / 2.0 <=  this.posY + Settings.paddle.height / 2.0 && 
-                ball.posY + Settings.ball.width / 2.0 >=  this.posY + Settings.paddle.height / 2.0) {
-          ball.DirX = -ball.DirX;
-          ball.DirY = -this.posY * 0.7;
-        }
-    }
-}
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibuff);
+		gl.drawElements(gl.TRIANGLES, this.ibuff.number_vertex_points, gl.UNSIGNED_SHORT, 0);
+
+		WebGLUtils.popMatrix();
+	};
+
+	function Ball(width, height, x, y, speed, vbuff, ibuff, txtbuff, txtid, nbuff) {
+		ObjGraph.call(this, width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff);
+		this.DirX = 1;
+		this.DirY = 1;
+		this.speed = speed;
+	};
+	Ball.prototype = new ObjGraph();
+
+	Ball.prototype.move = function() {
+		var limitX = Settings.field.width / 2.0 - 0.8*this.width;
+		var limitY = Settings.field.height / 2.0 - 0.8*this.width;
+
+		if (this.posX >= limitX || this.posX <= -limitX) {
+			this.DirX = -this.DirX;
+		}
+		if (this.posY >= limitY || this.posY <= -limitY) {
+			this.DirY = -this.DirY;
+		}
+
+		// update ball position
+		this.posX += this.DirX * this.speed;
+		this.posY += this.DirY * this.speed;
+	}
+
+	function Paddle(width, height, x, y, speed, vbuff, ibuff, txtbuff, txtid, nbuff) {
+		ObjGraph.call(this, width, height, x, y, vbuff, ibuff, txtbuff, txtid, nbuff);
+		this.speed = speed;
+	}
+	Paddle.prototype = new ObjGraph();
+
+	Paddle.prototype.move = function() {
+		var limitX = Settings.field.width / 2.0 - this.width;
+
+		// move right
+		if (Key.isDown(Key.RIGHT) || (Key.isDown(Key.D))) {
+			if (this.posX <= limitX) {
+				// update paddle position
+				this.posX += this.speed;
+			}
+		}
+		// move left
+		else if (Key.isDown(Key.LEFT) || (Key.isDown(Key.A))) {
+			if (this.posX >= -limitX) {
+				// update paddle position
+				this.posX -= this.speed;
+			}
+		}
+	};
+
+	Paddle.prototype.logic = function() {
+		if (ball.posX <= this.posX + Settings.paddle.width / 2.0  &&
+			ball.posX >= this.posX - Settings.paddle.width / 2.0) {
+				if (ball.posY - Settings.ball.width / 2.0 <=  this.posY + Settings.paddle.height / 2.0 && 
+					ball.posY + Settings.ball.width / 2.0 >=  this.posY + Settings.paddle.height / 2.0) {
+						ball.DirX = -ball.DirX;
+						ball.DirY = -this.posY * 0.7;
+				}
+		}
+	};
+
+	module.start = function() {
+		gl = WebGLUtils.getGL();
+		glProgram = WebGLUtils.getGLProgram();
+		textures = WebGLUtils.getGLTextures();
+
+		matrix = WebGLUtils.getMatrix();
+
+		var buffers = WebGLBuffers.get();
+
+		objects.field = new ObjGraph(Settings.field.width, Settings.field.height, 0.0, 0.0, 
+				buffers.field.vertex, buffers.field.vindex, buffers.field.vtexture, Settings.field.texture, 
+				buffers.field.vnormal);
+
+		objects.ball = new Ball(Settings.ball.width, Settings.ball.width, 0.0, 0.0, Settings.ball.speed, 
+				buffers.ball.vertex, buffers.ball.vindex, buffers.ball.vtexture, Settings.ball.texture,
+				buffers.ball.vnormal);
+
+		objects.paddle1 = new Paddle(Settings.paddle.width, Settings.paddle.height, 0.0,
+				-Settings.field.height / 2.0 + 3*Settings.paddle.height, Settings.paddle.speed,
+				buffers.paddle.vertex, buffers.paddle.vindex, buffers.paddle.vtexture, 
+				Settings.paddle.texture1, buffers.paddle.vnormal);
+
+		objects.paddle2 = new Paddle(Settings.paddle.width, Settings.paddle.height, 0.0,
+				Settings.field.height / 2.0 - 5*Settings.paddle.height, Settings.paddle.speed,
+				buffers.paddle.vertex, buffers.paddle.vindex, buffers.paddle.vtexture,
+				Settings.paddle.texture2, buffers.paddle.vnormal);
+
+		return objects;
+  };
+
+	return module;
+}());
