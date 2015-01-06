1f0313e3df62385f88754036c6f21cb413298fce
Jorge Campos <jorge.camposserrano@gmail.com>
2015-01-06 18:34:18 +0100

    Change indentation in keyboard module

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,24 +1,26 @@
+/**
+ * Keyboard Module
+ */
+var Key = {
+	 _pressed: {},
+
+	LEFT: 37,
+	RIGHT: 39,
+	A: 65,
+	D: 68,
+
+	isDown: function(keyCode) {
+		return this._pressed[keyCode];
+	},
+
+	onKeydown: function(event) {
+		this._pressed[event.keyCode] = true;
+	},
+
+	onKeyup: function(event) {
+		delete this._pressed[event.keyCode];
+	}
+};
 
 window.addEventListener("keydown", function(event) { Key.onKeydown(event); }, false);
 window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
-
-var Key = {
-   _pressed: {},
-
-  LEFT: 37,
-  RIGHT: 39,
-  A: 65,
-  D: 68,
-
-  isDown: function(keyCode) {
-    return this._pressed[keyCode];
-  },
-
-  onKeydown: function(event) {
-    this._pressed[event.keyCode] = true;
-  },
-
-  onKeyup: function(event) {
-    delete this._pressed[event.keyCode];
-  }
-};
